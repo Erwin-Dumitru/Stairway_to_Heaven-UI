b812d3363a78cd447cfc946d4ed94579a4aaed45
Tristan Dumitru <dumitruerwin@yahoo.com>
2024-03-05 20:43:04 +0200

    Return from bun to npm :(

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 
 // const root = resolve(__dirname, 'src')
 const root = __dirname
