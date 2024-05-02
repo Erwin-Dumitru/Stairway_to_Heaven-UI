a6f5d3f5c5be76cf3013196abc650b3147ab06df
Tristan Dumitru <dumitruerwin@yahoo.com>
2024-05-02 12:48:50 +0300

    Association Home updates

--- src/dashboard/home/homeAssoc/raports/expenses/Expenses.tsx
+++ src/dashboard/home/homeAssoc/raports/expenses/Expenses.tsx
@@ -1,4 +1,6 @@
 import './Expenses.scss';
+import Counter from '@/assets/Counter.png';
+import Distribution from '@/assets/Distribution.png';
 
 interface DataType {
     group: string;
@@ -37,11 +39,28 @@ export default function Expenses({ data, stair, show }: { data: DataType[], stai
             <div className="expenses-list">
                 <div className='expense header'>
                     <input type="checkbox" />
-                    <h3>Nume</h3>
-                    <h3>Tip</h3>
-                    <h3>Unitate</h3>
-                    <h3>Individual</h3>
-                    <h3>Total</h3>
+                    {/* <div className="type"></div> */}
+                    {/* <h3>Tip</h3> */}
+
+                    <div className="field">
+                        <h3>Nume</h3>
+                    </div>
+
+                    <div className="field">
+                        <h3>Apartamente</h3>
+                    </div>
+
+                    <div className="field">
+                        <h3>Unitate</h3>
+                    </div>
+
+                    <div className="field">
+                        <h3>Individual</h3>
+                    </div>
+
+                    <div className="field">
+                        <h3>Total</h3>
+                    </div>
                     <div className="add">
                         <i className="ri-add-line"></i>
                     </div>
@@ -53,13 +72,52 @@ export default function Expenses({ data, stair, show }: { data: DataType[], stai
                         <div className={`expense ${index % 2 === 0 ? 'even' : 'odd'}`} 
                             key={index}>
                             <input type="checkbox" />
-                            {/* <span>{expense.group}</span> */}
-                            <h3>{expense.name}</h3>
-                            <h3>{expense.kind}</h3>
-                            <h3>{expense.unit}</h3>
-                            <h3>{expense.individualAmount.toFixed(2)}</h3>
-                            <h3>{expense.totalAmount.toFixed(2)}</h3>
-                            {/* {expense.finished ? <i className="ri-checkbox-circle-fill"></i> : <i className="ri-close-circle-fill"></i>} */}
+                            <div className="field">
+                                <div className="type">
+                                    <img src={expense.kind === 'Contorizat' ? Counter : Distribution} alt="type" />
+                                </div>
+                                
+                                <h3>{expense.name}</h3>
+                            </div>
+
+                            <div className="field selector">
+                                <div className="selected">
+                                    <h3>15/48</h3>
+                                </div>
+
+                                <div className="dropdown">
+                                    <div className="groups">
+                                        <div className="group">
+                                            <h3>Tot</h3>
+                                        </div>
+                                    </div>
+
+                                    <div className="apartments">
+                                        <div className="apartment">
+                                            <h3>15</h3>
+                                        </div>
+
+                                        <div className="apartment">
+                                            <h3>16</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div className="field">
+                                <h3>{expense.unit}</h3>
+                            </div>
+
+                            <div className="field">
+                                {/* <h3>{expense.individualAmount.toFixed(2)}</h3> */}
+                                <input type="number" value={expense.individualAmount.toFixed(2)} />
+                            </div>
+
+                            <div className="field">
+                                {/* <h3>{expense.totalAmount.toFixed(2)}</h3> */}
+                                <input type="number" value={expense.totalAmount.toFixed(2)} />
+                            </div>
+                            
                             <div className="add">
                                 <h4>••</h4>
                             </div>
